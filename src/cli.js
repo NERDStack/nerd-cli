ff54f00c104bf806390e6d9db3dcc452f8a62666
Thomas Stringer <tr.stringer@outlook.com>
2016-10-20 19:57:29 -0400

    add initial base cli commands

--- src/cli.js
+++ src/cli.js
@@ -1,2 +1,21 @@
-console.log('hello, nerd stack!');
+const program = require('commander');
+const package = require('../package.json');
+
+program.version(package.version);
+
+program
+  .command('install')
+  .action(() => {
+    console.log('install operation');
+  })
+  .description('install the base app');
+
+program
+  .command('cleanup')
+  .action(() => {
+    console.log('cleanup operation');
+  })
+  .description('remove all unnecessary sample code');
+
+program.parse(process.argv);
 
