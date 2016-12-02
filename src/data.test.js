4b332db8165a6798d7d78b0ed714e3844ac059aa
Thomas Stringer <tr.stringer@outlook.com>
2016-12-02 07:56:05 -0500

    wrote failing test for collection creation

--- src/data.test.js
+++ src/data.test.js
@@ -1,8 +1,15 @@
+const data = require('./data');
 
 /* eslint-disable no-undef */
 
 it('documentdb collection is created', () => {
-
+  const docdbUri = process.env.DOCUMENTDB_URI;
+  const docdbKey = process.env.DOCUMENTDB_KEY;
+  return data.createCollection(docdbUri, docdbKey)
+    .then(
+      res => expect(res).toBeTruthy(),
+      err => expect(err).toBeNull()
+    );
 });
 
 /* eslint-enable no undef */
