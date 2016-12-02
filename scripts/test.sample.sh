3803899c8b273ac9ab2905c3f8fa0e9fbcfa4f2a
Thomas Stringer <tr.stringer@outlook.com>
2016-12-02 07:47:42 -0500

    add shell wrapper for test env vars

---
+++ scripts/test.sample.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# In order to run the tests for this project you must do the following:
+#  1. Copy scripts/test.sample.sh to scripts/test.sh
+#  2. Add the DocumentDB URI and keys as variables below
+
+DOCUMENTDB_URI='...' DOCUMENTDB_KEY='...' jest
+
---
+++ src/data.test.js
@@ -0,0 +1,9 @@
+
+/* eslint-disable no-undef */
+
+it('documentdb collection is created', () => {
+
+});
+
+/* eslint-enable no undef */
+
