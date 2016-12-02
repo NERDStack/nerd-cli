cc459f4ff2a4ce82de3304b0a5e9b164760e0dfe
Thomas Stringer <tr.stringer@outlook.com>
2016-12-02 11:35:52 -0500

    add functionality to seed data

--- src/data.js
+++ src/data.js
@@ -1,15 +1,56 @@
 const DocumentClient = require('documentdb').DocumentClient;
 
-module.exports.createCollection = (documentdbUri, documentdbKey, dbName, collName) => {
+function getCollectionLink(documentdbUri, documentdbKey, connInfo) {
+  return new Promise((resolve, reject) => {
+    const client = new DocumentClient(documentdbUri, { masterKey: documentdbKey });
+    const querySpecDb = {
+      query: 'select * from root r where r.id = @id',
+      parameters: [{
+        name: '@id',
+        value: connInfo.databaseName
+      }]
+    };
+    client.queryDatabases(querySpecDb).toArray((err, results) => {
+      if (err) {
+        reject(err);
+      }
+      else if (results.length === 0) {
+        reject(Error(`Database ${connInfo.databaseName} not found`));
+      }
+      else {
+        const querySpecColl = {
+          query: 'select * from root r where r.id = @id',
+          parameters: [{
+            name: '@id',
+            value: connInfo.collectionName
+          }]
+        };
+        client.queryCollections(results[0]._self, querySpecColl).toArray((err, results) => {
+          if (err) {
+            reject(err);
+          }
+          else if (results.length === 0) {
+            reject(Error(`Collection ${connInfo.collectionName} not found in database ${connInfo.databaseName}`));
+          }
+          else {
+            resolve(results[0]);
+          }
+        });
+      }
+    });
+  });
+}
+
+module.exports.createCollection = (documentdbUri, documentdbKey, connInfo) => {
   return new Promise((resolve, reject) => {
     const client = new DocumentClient(documentdbUri, { masterKey: documentdbKey });
 
-    client.createDatabase({ id: dbName }, (err, database) => {
+    client.createDatabase({ id: connInfo.databaseName }, (err, database) => {
       if (err) {
         reject(err);
       }
       else {
-        client.createCollection(database._self, { id: collName }, (err, collection) => {
+        client.createCollection(database._self, { id: connInfo.collectionName }, (err, collection) => {
           if (err) {
             reject(err);
           }
@@ -20,5 +61,29 @@ module.exports.createCollection = (documentdbUri, documentdbKey, dbName, collNam
       }
     });
   });
+};
+
+module.exports.seedSampleData = (documentdbUri, documentdbKey, connInfo, sampleData) => {
+  return getCollectionLink(documentdbUri, documentdbKey, connInfo)
+    .then(
+      collLink => {
+        const client = new DocumentClient(documentdbUri, { masterKey: documentdbKey });
+        return Promise.all(
+          sampleData.map(insertDoc => {
+            return new Promise((resolve, reject) => {
+              client.createDocument(collLink._self, insertDoc, (err, insertedDoc) => {
+                if (err) {
+                  reject(err);
+                }
+                else {
+                  resolve(insertedDoc);
+                }
+              });
+            });
+          })
+        );
+      },
+      err => { throw err; }
+    );
 };
 
--- src/data.test.js
+++ src/data.test.js
@@ -1,5 +1,6 @@
 const DocumentClient = require('documentdb').DocumentClient;
 const data = require('./data');
+const sampleData = require('../sample-data/seed-data');
 
 const docdbUri = process.env.DOCUMENTDB_URI;
 const docdbKey = process.env.DOCUMENTDB_KEY;
@@ -8,11 +9,51 @@ const collectionName = 'clitestcoll';
 
 /* eslint-disable no-undef */
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
+
 it('documentdb collection is created', () => {
-  return data.createCollection(docdbUri, docdbKey, databaseName, collectionName)
+  return data.createCollection(docdbUri, docdbKey, { databaseName, collectionName })
     .then(
       res => expect(res).toBeTruthy(),
       err => expect(err).toBeNull()
+    );
+});
+
+it('sample data is inserted', () => {
+  return data.seedSampleData(
+    docdbUri, docdbKey, { databaseName, collectionName }, sampleData.seedData)
+    .then(
+      () => {
+        const client = new DocumentClient(docdbUri, { masterKey: docdbKey });
+        const querySpecDb = {
+          query: 'select * from root r where r.id = @id',
+          parameters: [{
+            name: '@id',
+            value: databaseName
+          }]
+        };
+        client.queryDatabases(querySpecDb).toArray((err, results) => {
+          expect(err).toBeFalsy();
+          expect(results.length).toBeGreaterThan(0);
+          const querySpecColl = {
+            query: 'select * from root r where r.id = @id',
+            parameters: [{
+              name: '@id',
+              value: collectionName
+            }]
+          };
+          client.queryCollections(results[0]._self, querySpecColl, (err, results) => {
+            expect(err).toBeFalsy();
+            expect(results.length).toBeGreaterThan(0);
+            client.queryDocuments(results[0]._self, 'select * from root', (err, results) => {
+              expect(err).toBeFalsy();
+              expect(results.length).toBeGreaterThan(0);
+              resolve();
+            });
+          });
+        });
+      },
+      err => expect(err).toBeFalsy()
     );
 });
 
