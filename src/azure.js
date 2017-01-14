9f0428199ecec82fcf78dbaee62a148dfe2378c1
Thomas Stringer <tr.stringer@outlook.com>
2017-01-14 07:46:21 -0500

    (wip) retrieve azure regions

--- src/azure.js
+++ src/azure.js
@@ -1,7 +1,48 @@
+const readline = require('readline');
+const msRestAzure = require('ms-rest-azure');
+const resourceManagement = require('azure-arm-resource');
+
 module.exports.publish = () => {
   console.log('yay azure...');
 };
 
 module.exports.listRegions = () => {
-  console.log('regions...');
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    rl.question('Your subscription ID: ', subscriptionId => {
+      if (!subscriptionId) {
+        rl.close();
+        reject(Error('No input detected'));
+        return;
+      }
+      rl.question('Your tenant ID [optional]: ', tenantId => {
+        console.log(`Subscription ID is ${subscriptionId}`);
+        if (tenantId) {
+          console.log(`Tenant ID is ${tenantId}`);
+        }
+        else {
+          console.log('No tenant ID inputed');
+        }
+
+        msRestAzure.interactiveLogin({ domain: tenantId }, (err, creds, subs) => {
+          const client = new resourceManagement.SubscriptionClient(creds);
+          client.subscriptions.listLocations(subscriptionId, (err, result, request, response) => {
+            if (err) {
+              console.log(`Error as follows:\n${err}`);
+              rl.close();
+              reject(err);
+              return;
+            }
+            // console.log(result);
+            console.log(subs);
+            rl.close();
+            resolve();
+          });
+        });
+      });
+    });
+  });
 };
