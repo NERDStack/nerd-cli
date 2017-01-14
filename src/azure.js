596a1d46faf4a9f499253dfc46c31df27247afab
Thomas Stringer <tr.stringer@outlook.com>
2017-01-14 07:50:08 -0500

    refactor out the need to prompt for subscription id

--- src/azure.js
+++ src/azure.js
@@ -12,35 +12,25 @@ module.exports.listRegions = () => {
       input: process.stdin,
       output: process.stdout
     });
-    rl.question('Your subscription ID: ', subscriptionId => {
-      if (!subscriptionId) {
-        rl.close();
-        reject(Error('No input detected'));
-        return;
-      }
-      rl.question('Your tenant ID [optional]: ', tenantId => {
-        console.log(`Subscription ID is ${subscriptionId}`);
-        if (tenantId) {
-          console.log(`Tenant ID is ${tenantId}`);
+    rl.question('Your tenant ID [optional]: ', tenantId => {
+      msRestAzure.interactiveLogin({ domain: tenantId }, (err, creds, subs) => {
+        if (!subs || subs.length === 0) {
+          reject(Error('Unable to retrieve subscriptions'));
+          rl.close();
+          return;
         }
-        else {
-          console.log('No tenant ID inputed');
-        }
-
-        msRestAzure.interactiveLogin({ domain: tenantId }, (err, creds, subs) => {
-          const client = new resourceManagement.SubscriptionClient(creds);
-          client.subscriptions.listLocations(subscriptionId, (err, result, request, response) => {
-            if (err) {
-              console.log(`Error as follows:\n${err}`);
-              rl.close();
-              reject(err);
-              return;
-            }
-            // console.log(result);
-            console.log(subs);
+        const client = new resourceManagement.SubscriptionClient(creds);
+        client.subscriptions.listLocations(subs[0].id, (err, result, request, response) => {
+          if (err) {
+            console.log(`Error as follows:\n${err}`);
             rl.close();
-            resolve();
-          });
+            reject(err);
+            return;
+          }
+          // console.log(result);
+          console.log(subs);
+          rl.close();
+          resolve();
         });
       });
     });
