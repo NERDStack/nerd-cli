a45e98022d18b354ab3d890dea71ac88a2dbd676
Thomas Stringer <tr.stringer@outlook.com>
2017-01-19 06:35:46 -0500

    (wip) remove nodegit dependency

--- src/install.js
+++ src/install.js
@@ -1,10 +1,17 @@
 const exec = require('child_process').exec;
 const path = require('path');
-const Git = require('nodegit');
 
 function cloneRepo(destination) {
-  const repoUrl = 'https://github.com/NERDStack/nerdy-movies';
-  return Git.Clone(repoUrl, destination);
+  return new Promise((resolve, reject) => {
+    const repoUrl = 'https://github.com/NERDStack/nerdy-movies';
+    exec(`git clone ${repoUrl} ${destination}`, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
 }
 
 function fullWorkingDirectory(dir) {
@@ -25,6 +32,7 @@ module.exports = (dir) => {
           }
         });
       });
-    });
+    })
+    .catch(err => console.log(err.message));
 };
 
