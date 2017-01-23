97266151dca8ba02604e5aa04f5f18320a3032be
Thomas Stringer <tr.stringer@outlook.com>
2017-01-23 13:31:06 -0500

    add ability to detect platform and run correct start local npm script

--- src/run.js
+++ src/run.js
@@ -2,7 +2,8 @@ const exec = require('child_process').exec;
 
 module.exports = () => {
   return new Promise((resolve, reject) => {
-    exec('npm run build && npm run start-local', err => {
+    const runScriptName = process.platform === 'win32' ? 'start-local-win' : 'start-local';
+    exec(`npm run build && npm run ${runScriptName}`, err => {
       if (err) {
         reject(err);
       }
